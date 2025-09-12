c3c41841ae4f339316e304c605733ee45b4251bf
Cameron <cameron_2000@live.co.uk>
2025-09-12 17:29:38 +0100

    get project ready for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
+    'https://portfolio-back-end-c6cab71f887f.herokuapp.com',
     'localhost',
     '127.0.0.1',
 ]
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,7 +149,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
