c94853719ed03f5fb3e376bf78e2df9f79251233
Cameron <cameron_2000@live.co.uk>
2025-09-12 19:56:03 +0100

    add ManyToMany field within projects

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.conf import settings
+from languages.models import Language
 
 # Create your models here.
 
@@ -10,15 +10,18 @@ class Project(models.Model):
         on_delete=models.CASCADE,
         related_name="projects"
     )
-    name = models.CharField(blank=False)
-    description = models.TextField()
-    languages = models.CharField()
-    live_link = models.CharField(blank=False)
-    repo_link = models.CharField(blank=False)
-    thumbnail = models.ImageField(upload_to="projects", blank=True)
+    name = models.CharField(blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    languages = models.ManyToManyField(
+        Language,
+        related_name="projects",
+        )
+    live_link = models.URLField(blank=False, null=False)
+    repo_link = models.URLField(blank=False, null=False)
+    thumbnail = models.ImageField(upload_to="projects", null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.name} created at {self.created_at}'
 
