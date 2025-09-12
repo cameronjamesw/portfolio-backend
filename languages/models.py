1a3c3bce6ee7cd5676b27302836d03d3dfe93288
Cameron <cameron_2000@live.co.uk>
2025-09-12 19:44:59 +0100

    create languages model

---
+++ languages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(blank=False)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="languages",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        self.name
