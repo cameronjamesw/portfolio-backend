74c07dd1cfb0d09bd6a73b645f490457fc5df50f
Cameron <cameron_2000@live.co.uk>
2025-09-15 22:10:44 +0100

    set debug to false

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -35,7 +35,7 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
