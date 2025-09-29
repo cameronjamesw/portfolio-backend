8d2d685eb1a68c53120382c94d2b8e7567c37bff
Cameron <cameron_2000@live.co.uk>
2025-09-29 23:07:13 +0100

    create first project view

--- api/projects/views.py
+++ api/projects/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Project
+from .serializers import ProjectSerializer
 
 # Create your views here.
+
+@csrf_exempt
+def project_list(request):
+    """
+    Lists all the projects, or creates a new one
+    """
+
+    if request.method == "GET":
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
