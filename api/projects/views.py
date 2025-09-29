416dbbf665f80366b9e6f6ea2a691f32c51ba64b
Cameron <cameron_2000@live.co.uk>
2025-09-29 23:19:21 +0100

    create project_detail view

--- api/projects/views.py
+++ api/projects/views.py
@@ -25,3 +25,32 @@ def project_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def project_detail(request, pk):
+    """
+    Retrieve, update or delete a project
+    """
+
+    try:
+        project = Project.objects.get(pk=pk)
+    except Project.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method == "GET":
+        serializer = ProjectSerializer(project)
+        return JSONParser(serializer.data)
+    
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ProjectSerializer(project, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, response=200)
+        return JsonResponse(serializer.errors, status=400)
+    
+    elif request.method == "DELETE":
+        project.delete()
+        return HttpResponse(status=204)
+        
