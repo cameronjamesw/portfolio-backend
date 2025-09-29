7607f78f6828ce1a4733aa606a1e5af55f497b46
Cameron <cameron_2000@live.co.uk>
2025-09-29 22:19:52 +0100

    create basic project serializer

---
+++ api/projects/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            'user',
+            'name',
+            'description',
+            'languages',
+            'image',
+            'live_link',
+            'repo_link',
+            'thumbnail',
+            'created_at',
+            'updated_at'
+        ]
