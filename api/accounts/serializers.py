13c7e25ca6e6d639fa5c460e1eb0cceee3e2e960
Cameron <cameron_2000@live.co.uk>
2025-10-01 22:07:26 +0100

    create CustomUserSerializer

---
+++ api/accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import CustomUser
+from api.projects.models import Project
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    projects = serializers.PrimaryKeyRelatedField(many=True, queryset=Project.objects.all())
+
+    class Meta:
+        model = CustomUser
+        field = [
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'bio',
+            'profile_image',
+            'projects',
+        ]
