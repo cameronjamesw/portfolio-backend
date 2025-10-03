a5524c17bcbf0054b126559fb1f6763ad82ecdf1
Cameron <cameron_2000@live.co.uk>
2025-10-03 10:36:18 +0100

    add further fields to project model

--- api/projects/models.py
+++ api/projects/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from api.languages.models import Language
+from api.frameworks.models import Framework
+from api.libraries.models import Library
 
 # Create your models here.
 
@@ -11,12 +13,23 @@ class Project(models.Model):
         related_name="projects"
     )
     name = models.CharField(blank=True, null=True)
+    collaboration = models.BooleanField(default=False)
     description = models.TextField(blank=True, null=True)
     languages = models.ManyToManyField(
         Language,
         related_name="projects",
         blank=True
         )
+    libraries = models.ManyToManyField(
+        Library,
+        related_name="projects",
+        blank=True
+    )
+    frameworks = models.ManyToManyField(
+        Framework,
+        related_name="projects",
+        blank=True
+    )
     image = models.ImageField(
         upload_to='images/',
         default='../default_image_nyj9lt',
