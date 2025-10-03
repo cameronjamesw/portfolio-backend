b17adcc6624a8f8ee8e68de7e08a41116a971fc9
Cameron <cameron_2000@live.co.uk>
2025-10-03 10:22:50 +0100

    create framework model

--- api/frameworks/apps.py
+++ api/frameworks/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class FrameworksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'frameworks'
+    name = 'api.frameworks'
+    label = 'frameworks'
--- api/frameworks/models.py
+++ api/frameworks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from portfolio import settings
 
 # Create your models here.
+
+class Framework(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        related_name = 'frameworks'
+    )
+    name = models.CharField(
+        max_length = 20,
+        blank = False,
+        unique = True
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
