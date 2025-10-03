9f042597eb24385826e20d38ec15dd92762ea545
Cameron <cameron_2000@live.co.uk>
2025-10-03 10:11:07 +0100

    create library model

--- api/libraries/models.py
+++ api/libraries/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from portfolio import settings
 
 # Create your models here.
+
+class Library(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="libraries"
+    )
+    name = models.CharField(
+        blank=False, unique=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
