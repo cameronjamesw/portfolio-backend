dc3e1ef7583d99642ca30dab72798a11e3baa773
Cameron <cameron_2000@live.co.uk>
2025-10-03 11:50:55 +0100

    add comments to language apps

--- api/languages/views.py
+++ api/languages/views.py
@@ -6,6 +6,10 @@ from .serializers import LanguageSerializer
 # Create your views here.
 
 class LanguageList(generics.ListCreateAPIView):
+    """
+    This view fetches all the language objects and
+    returns them to the user
+    """
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
     permission_classes = [IsAdminUserOrReadOnly]
@@ -14,6 +18,10 @@ class LanguageList(generics.ListCreateAPIView):
         serializer.save(self.request.user)
 
 class LanguageDetail(generics.RetrieveDestroyAPIView):
+    """
+    This view fetches specific language objects and
+    returns them to the user
+    """
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
     permission_classes = [IsAdminUserOrReadOnly]
